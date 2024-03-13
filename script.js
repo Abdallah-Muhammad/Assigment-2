822422b4cae444bcf2a6edaaaab5a599c1d8e105
Abdallah-Muhammad <160315873+Abdallah-Muhammad@users.noreply.github.com>
2024-03-13 14:56:37 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+
+alert('hello, World');
+document.addEventListener('DOMContentLoaded', function () {
+    // DOMContentLoaded event ensures that the script runs after the HTML has been completely loaded
+
+    // Select the header element
+    var header = document.querySelector('header h1');
+
+    // Change the color of the header to red
+    header.style.color = 'red';
+});
